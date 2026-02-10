31c2499bff492c9513499806d18e5413fc25ab8a
Anton Zellman <antonzellman@MacBook-Air-som-tillhor-Anton.local>
2026-02-10 18:34:47 +0100

    Adjust Vite build target for Safari

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2017',
+    minify: false,
+  },
+  esbuild: {
+    target: 'es2017',
+  },
 })
